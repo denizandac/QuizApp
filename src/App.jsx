45244deffbf6becdf31a7d51df51b8fd884f205b
Deniz <denizandac33@gmail.com>
2024-03-02 21:23:06 +0300

    Component Update
    - Removed QuestionTimer

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import { useRef, useState } from "react";
 import QuestionTimer from "./components/QuestionTimer.jsx";
 import Modal from "./components/Modal.jsx";
 import QuestionTimeSelect from "./components/QuestionTimeSelect.jsx";
-import quizLogo from "./assets/quiz-logo.png";
+import Header from "./components/Header.jsx";
+import Quiz from "./components/Quiz.jsx";
 
 function App() {
-  const [questionTime, setQuestionTime] = useState(10000);
-  const [activeQuestion, setActiveQuestion] = useState();
   const modalRef = useRef();
 
   const handleShowModal = () => {
@@ -15,13 +14,8 @@ function App() {
 
   return (
     <div className="App">
-      <QuestionTimeSelect onQuestionTimeSelect={setQuestionTime} />
-      <button onClick={handleShowModal}>Show Modal</button>
-      <Modal ref={modalRef}>
-        <h2>Modal Title</h2>
-        <p>Modal Body</p>
-        <QuestionTimer questionTime={questionTime} key={Math.random()} />
-      </Modal>
+      <Header />
+      <Quiz />
     </div>
   );
 }
